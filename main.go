654b02c4a482a6da5be33da0936f53b95b917fa1
casiojapi <f91w@tuta.io>
2024-10-20 03:24:49 -0300

    working solution, now it responds with the proof in json file

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
@@ -30,11 +32,13 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
+
 	bot.Debug = true
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
+
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
@@ -42,61 +46,91 @@ func main() {
 			continue
 		}
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-
 		switch update.Message.Command() {
 		case "proof":
-			memberID := fmt.Sprintf("%d", update.Message.From.ID)
-			proof, err := requestProof(memberID, memberID)
-			if err != nil {
-				msg.Text = "Error generating proof: " + err.Error()
-			} else {
-				msg.Text = "Your proof: \n" + proof
-			}
+			handleProofCommand(bot, update)
 		default:
-			msg.Text = "I don't know that command"
-		}
-
-		if _, err := bot.Send(msg); err != nil {
-			log.Panic(err)
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "I don't know that command")
+			bot.Send(msg)
 		}
 	}
 }
 
-func requestProof(member string, expectedMember string) (string, error) {
+// /proof endpoint handling
+func handleProofCommand(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+	// Use the Telegram user's ID as the member ID
+	memberID := fmt.Sprintf("%d", update.Message.From.ID)
+
+	// Request proof from the proving server
+	proof, err := requestProof(memberID, memberID)
+	if err != nil {
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Error generating proof: "+err.Error())
+		bot.Send(msg)
+		return
+	}
+
+	filename, err := saveProofAsJSON(proof)
+	if err != nil {
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Error saving proof to file: "+err.Error())
+		bot.Send(msg)
+		return
+	}
+	defer os.Remove(filename) 
+
+	// send json to user
+	file := tgbotapi.NewDocument(update.Message.Chat.ID, tgbotapi.FilePath(filename))
+	if _, err := bot.Send(file); err != nil {
+		log.Panic(err)
+	}
+}
+
+// proof generation
+func requestProof(member string, expectedMember string) (map[string]interface{}, error) {
+	
 	reqBody, err := json.Marshal(ProofRequest{
 		Member:         member,
 		ExpectedMember: expectedMember,
 	})
+	
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	resp, err := http.Post("http://localhost:8080/generate-proof", "application/json", bytes.NewBuffer(reqBody))
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	defer resp.Body.Close()
 
+	// check response
 	if resp.StatusCode != http.StatusOK {
 		body, _ := ioutil.ReadAll(resp.Body)
-		return "", fmt.Errorf("server error: %s", string(body))
+		return nil, fmt.Errorf("server error: %s", string(body))
 	}
 
 	var proofResp ProofResponse
 	if err := json.NewDecoder(resp.Body).Decode(&proofResp); err != nil {
+		return nil, err
+	}
+
+	return proofResp.Proof, nil
+}
+
+
+func saveProofAsJSON(proof map[string]interface{}) (string, error) {
+	filename := fmt.Sprintf("only_members_proof_%d.json", time.Now().Unix())
+
+	// Marshal the proof into JSON format
+	data, err := json.MarshalIndent(proof, "", "  ")
+	if err != nil {
 		return "", err
 	}
 
-	return convertProofToString(proofResp.Proof), nil
-}
-
-func convertProofToString(proof map[string]interface{}) string {
-	var result string
-	for i := 0; i < len(proof); i++ {
-		if val, exists := proof[fmt.Sprintf("%d", i)]; exists {
-			result += fmt.Sprintf("%v", val)
-		}
+	// write json
+	err = ioutil.WriteFile(filename, data, 0644)
+	if err != nil {
+		return "", err
 	}
-	return result
+
+	return filename, nil
 }
